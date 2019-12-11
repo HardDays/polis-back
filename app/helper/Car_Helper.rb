6db1412b18ce544130ce69539a06b74a5aba03b4
kparamonov <prmnr2k@gmail.com>
2019-12-11 16:48:34 +0300

    asd

--- app/helper/Car_Helper.rb
+++ app/helper/Car_Helper.rb
@@ -6,12 +6,14 @@ module CarHelper
     response = RestClient.get Helper.api_url.to_s, {'Authorization':token, 'Content-Type':'application/json', params:{q:'list_brands'}}
 
     JSON.parse(response.body)['results'].each do |brand|
-      @brand_m = CarBrand.new(title: brand.to_s)
-      if @brand_m.save
-        models = RestClient.get Helper.api_url.to_s, {'Authorization':token, 'Content-Type':'application/json', params:{q:'list_models', brand:brand}}
-        JSON.parse(models.body)['results'].each do |model|
-          @model_m = CarModel.new(title: model.to_s, car_brand_id:@brand_m.id.to_i)
-          @model_m.save
+      if /$([a-zA-Z]+[\s\-\_]?[a-zA-Z])+|ВАЗ|ЗАЗ|ГАЗ^/ === brand
+        @brand_m = CarBrand.new(title: brand.to_s)
+        if @brand_m.save
+          models = RestClient.get Helper.api_url.to_s, {'Authorization':token, 'Content-Type':'application/json', params:{q:'list_models', brand:brand}}
+          JSON.parse(models.body)['results'].each do |model|
+            @model_m = CarModel.new(title: model.to_s, car_brand_id:@brand_m.id.to_i)
+            @model_m.save
+          end
         end
       end
     end
